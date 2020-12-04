3e9a1f955f826af80944b3dae66004f02ce245d1
tmt-mateusz <tmt.ocb@gmail.com>
2020-12-04 02:19:02 +0100

    Add prices page, clean navbar, link-navbar, container look

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,7 @@ class PagesController < ApplicationController
 
   def about
   end
+
+  def prices
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-  get 'pages/about'
-  
   root 'pages#home'
   get '/about', to: 'pages#about'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/prices', to: 'pages#prices'
 end
