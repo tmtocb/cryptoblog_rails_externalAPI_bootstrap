99f69de2b66989ac511b9424476a4dec73192c55
tmt-mateusz <tmt.ocb@gmail.com>
2020-12-04 03:15:04 +0100

    Call external cryptocompare API and pick data to make news page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,13 @@
 class PagesController < ApplicationController
   def home
+  	require 'net/http'
+  	require 'json'
+
+  	@url = 'https://min-api.cryptocompare.com/data/v2/news/?lang=EN'
+  	@uri = URI(@url)
+  	@response = Net::HTTP.get(@uri)
+  	@news = JSON.parse(@response)
+  	
   end
 
   def about
