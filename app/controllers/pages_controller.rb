1e610e859d24ee91077ae71f4169406a2844e130
tmt-mateusz <tmt.ocb@gmail.com>
2020-12-04 04:03:41 +0100

    Add second API for crypto prices, use .each loop to run and take data

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,21 @@
 class PagesController < ApplicationController
   def home
+
   	require 'net/http'
   	require 'json'
 
+  	#Gets news API data
   	@url = 'https://min-api.cryptocompare.com/data/v2/news/?lang=EN'
   	@uri = URI(@url)
   	@response = Net::HTTP.get(@uri)
   	@news = JSON.parse(@response)
-  	
+
+  	#Gets prices API data
+   	@prices_url = 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,XRP,USDT,LTC,LINK,BCH,ADA,DOT,BNB&tsyms=USD'
+  	@prices_uri = URI(@prices_url)
+  	@prices_response = Net::HTTP.get(@prices_uri)
+  	@prices = JSON.parse(@prices_response)
+
   end
 
   def about
